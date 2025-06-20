5426a9b70cf5f8669051e2c410308a89eabbc7b9
DAEJIN JEONG <jjdj0315@gmail.com>
2025-06-20 17:17:23 +0900

    first commit

---
+++ project/utils/RiskAssessment.py
@@ -0,0 +1,30 @@
+class RiskAssessment:
+    def __init__(self, 인자1, 인자2):
+        self.인자1 = 인자1
+        self.인자2 = 인자2
+
+    @classmethod
+    def load(self):
+        """
+        데이터 로드
+        Parameters:
+            -path : 데이터 경로
+
+        Return:
+            -df : 경로에 위치한 데이터를 통합한 데이터 프레임
+        """
+        # 동작 정의
+        pass
+
+    def generate(self):
+        """
+        후보항목 생성
+        Parameters:
+            -path : 데이터 경로
+
+        Return:
+            -df : 경로에 위치한 데이터를 통합한 데이터 프레임                
+        """
+
+        pass
+
